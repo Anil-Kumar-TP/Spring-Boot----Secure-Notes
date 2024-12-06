a6c25560f990d4e8a3696a200982631cfd1d5fb1
Anil <anil6996f@gmail.com>
2024-12-06 20:33:18 +0530

    added url based restrictions for admin in security config

--- src/main/java/com/notes/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/notes/secure/notes/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.notes.secure.notes.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,8 +25,9 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-//        http.formLogin(withDefaults());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
         http.httpBasic(withDefaults());
         return http.build();
