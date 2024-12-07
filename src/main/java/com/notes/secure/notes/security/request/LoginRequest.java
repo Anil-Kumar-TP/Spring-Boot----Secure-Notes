88c6246fd98b25705c6fd480f3df53d28f0ac947
Anil <anil6996f@gmail.com>
2024-12-07 18:01:52 +0530

    added AuthController for SignIn and also the Login req and Login res

---
+++ src/main/java/com/notes/secure/notes/security/request/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.notes.secure.notes.security.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String username;
+    private String password;
+}
