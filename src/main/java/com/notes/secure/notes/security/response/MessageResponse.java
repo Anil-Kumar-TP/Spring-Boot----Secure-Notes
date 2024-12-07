bccf1ed2051904428ebbcb7654069ffc6802c519
Anil <anil6996f@gmail.com>
2024-12-07 20:32:31 +0530

    set up the dtos for signup req and res

---
+++ src/main/java/com/notes/secure/notes/security/response/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.notes.secure.notes.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+}
