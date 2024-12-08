bc17121b0222da594a70562f4554fa1d535dc287
Anil <anil6996f@gmail.com>
2024-12-08 21:18:43 +0530

    added more endpoints to admin controller that gives additional abilities to admin

--- src/main/java/com/notes/secure/notes/service/UserService.java
+++ src/main/java/com/notes/secure/notes/service/UserService.java
@@ -1,6 +1,7 @@
 package com.notes.secure.notes.service;
 
 import com.notes.secure.notes.dtos.UserDTO;
+import com.notes.secure.notes.model.Role;
 import com.notes.secure.notes.model.User;
 
 import java.util.List;
@@ -13,4 +14,16 @@ public interface UserService {
     UserDTO getUserById(Long id);
 
     User findByUsername(String username);
+
+    void updateAccountLockStatus(Long userId, boolean lock);
+
+    List<Role> getAllRoles();
+
+    void updateAccountExpiryStatus(Long userId, boolean expire);
+
+    void updateAccountEnabledStatus(Long userId, boolean enabled);
+
+    void updateCredentialsExpiryStatus(Long userId, boolean expire);
+
+    void updatePassword(Long userId, String password);
 }
