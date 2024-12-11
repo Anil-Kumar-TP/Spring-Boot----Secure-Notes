ba9b22b94eadf191d97521bf260a34a5210b1625
Anil <anil6996f@gmail.com>
2024-12-11 21:40:08 +0530

    added totpServiceImpl

---
+++ src/main/java/com/notes/secure/notes/service/TotpService.java
@@ -0,0 +1,11 @@
+package com.notes.secure.notes.service;
+
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+
+public interface TotpService {
+    GoogleAuthenticatorKey generateSecret();
+
+    String getQrCodeUrl(GoogleAuthenticatorKey secret, String username);
+
+    boolean verifyCode(String secret, int code);
+}
